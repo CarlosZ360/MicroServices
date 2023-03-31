e1a6479746f4bd296ba72d7ad61e26d5672da67f
CarlosZ360 <93692902+CarlosZ360@users.noreply.github.com>
2023-03-30 22:16:39 -0400

    Add files via upload

---
+++ ms-customer/src/main/kotlin/com/example/mscustomer/api/CustomerController.kt
@@ -0,0 +1,25 @@
+package com.example.mscustomer.api
+
+import com.example.mscustomer.service.AccountService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/customer")
+class CustomerController @Autowired constructor(private val accountService: AccountService){
+    @Value("7070")
+    lateinit var port: String
+    @GetMapping("/test")
+    fun test(): String {
+        val result= accountService.test()
+        return "server port: $port -> $result"
+    }
+    @GetMapping("/list")
+    fun getCustomers(): List<String> {
+        return listOf("Carlos","Conejo")+accountService.list()
+    }
+
+}
